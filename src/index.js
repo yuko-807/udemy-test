cecd88a780500fd72c23ac8c22dc3700a8e366ad
ゆーこ <75988330+yuko-807@users.noreply.github.com>
2021-02-03 08:07:05 +0900

    const,letについて

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
-import "./styles.css";
+/**
+ * const,let等の変数宣言
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var val1 ="var変数";
+//  console.log(val1);
+
+//  //var変数は上書き可能
+
+//  val1 ="var変数を上書き";
+//  console.log(val1);
+
+//  //var変数は再宣言可能
+//  var val1 ="var変数を再宣言";
+//  console.log(val1);
+
+// let val2 ='let変数';
+// console.log(val2);
+
+// letは再宣言が不可能・上書き可能
+
+// const変数は上書き・再宣言できない
+
+// constはオブジェクトの中だと上書きが可能
+
+// const val4 = {
+//   name:"neko",
+//   age:14,
+// };
+// val4.name="inu";
+// val4.address="kagoshima"
+// console.log(val4);
+
+// constで定義した配列はプロパティの変更が可能
+const val5 = ["dog", "cat"];
+val5[0] = "bird;";
+val5.push("monkey");
+console.log(val5);
